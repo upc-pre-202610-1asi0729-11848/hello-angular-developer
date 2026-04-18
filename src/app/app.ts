75ab91b0572b16261bb7315caded5e5d4c131fba
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-18 16:38:24 -0500

    feat(developer-registration): integrate developer registration component into main app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,27 @@
 import { Component, signal } from '@angular/core';
+import {
+  DeveloperRegistration
+} from './greetings/presentation/components/developer-registration/developer-registration';
 
 @Component({
   selector: 'app-root',
+  imports: [DeveloperRegistration],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('hello-angular-developer');
+  public firstName: string = '';
+  public lastName: string = '';
+
+  public updateRegisteredDeveloperInfo(developer: { firstName: string, lastName: string }): void {
+    this.firstName = developer.firstName;
+    this.lastName = developer.lastName;
+  }
+
+  public resetRegisteredDeveloperInfo(): void {
+    this.firstName = '';
+    this.lastName = '';
+  }
+
 }
